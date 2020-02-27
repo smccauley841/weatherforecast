3e7fc2b389752784f160604f056e37a3cf2dddbc
smccauley841 <smccauley841@gmail.com>
2020-02-27 00:26:08 +0000

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WeatherForecast.Models;
+
+namespace WeatherForecast.Controllers
+{
+    public class HomeController : Controller
+    {
+        public RootObject GetWeatherForecast()
+        {
+            string json = (new WebClient()).DownloadString("https://www.metaweather.com/api/location/44544/");
+            return JsonConvert.DeserializeObject<RootObject>(json);
+        }
+        public ActionResult Index()
+        {
+            var model = GetWeatherForecast();
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
